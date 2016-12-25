08c246460f8ca96f893a68174e4517663cce7e64
aalin <aaron.lin@stonybrook.edu>
2016-12-25 03:04:56 -0500

    Added reset functionality

--- eyescalc.py
+++ eyescalc.py
@@ -7,7 +7,11 @@
 # HOW TO DISPLAY SOLUTION
 # HOW TO GRAB ALL VALUES AND ASSIGN THEM THE CORRECT SHAPE+COLOR VALUE?
 # validatecommand for SpinBox
-# callback for button
+# command for button
+# selection_clear
+
+# THIS IS FOR CHECKING IF CURRENT INVENTORY HAS SOLUTION
+# WHAT IF PERSON HAS ITEMS THAT CAN BE CHANGED TO A SOLUTION?
 
 try:
     # Python 3.x
@@ -19,6 +23,23 @@ from PIL import Image, ImageTk
 
 colors = ['Red','Orange','Yellow','Green','Blue', 'Indigo', 'Violet']
 shapes = ['circle', 'triangle', 'square', 'pentagon']
+values = [1, 2, 3, 4, 5, 6, 7, 			# CORRESPONDING TO RED CIRCLE, ORANGE CIRCLE, YELLOW CIRCLE, ETC. IN ORDER
+			3, 6, 9, 12, 15, 18, 21, 
+			4, 8, 12, 16, 20, 24, 28,
+			5, 10, 15, 20, 25, 30, 35]
+
+def reset():
+	# ITERATE THROUGH THE DATA STUCTURE HOLDING ALL SPINBOXES AND ENTRY
+	# RESET ALL VALUES THEY HOLD
+	for widgets in spin_widgets:
+		widgets.delete(0, END)
+	for widgets in entry_widgets:
+		widgets.delete(0, END)
+
+def check():
+	# CHECK ALL SPINBOXES AND SEES IF WE HAVE SHAPES THAT SOLVE EACH LOCK
+	print "check"
+
 
 root = Tk()
 root.title("Eyes of Glouphrie Calculator")
@@ -31,7 +52,8 @@ canvas.pack()
 
 rows_count = 0
 columns_count = 0
-images = []
+spin_widgets = []
+entry_widgets = []
 
 # IMAGES AND SPIN BOXES
 for s in shapes:
@@ -58,6 +80,7 @@ for s in shapes:
 		shapecount = Spinbox(canvas, from_= 0, to = 50, width = 15)
 		# shapecount = Spinbox(canvas, from_= 0, to = 10, width = 15, background = "white")
 		shapecount.grid(row = rows_count, column = columns_count, padx = 5, pady = 5)
+		spin_widgets.append(shapecount)
 
 		columns_count += 1
 
@@ -96,15 +119,19 @@ fourth_lock.grid(row = rows_count, column = 1, sticky = E)
 fourth_entry = Entry(canvas, width = 10)
 fourth_entry.grid(row = rows_count, column = 2, pady = 5, sticky = W+E)
 
+entry_widgets.append(first_entry)
+entry_widgets.append(second_entry)
+entry_widgets.append(third_entry)
+entry_widgets.append(fourth_entry)
+
 # BUTTONS
-# command=callback
 
-rows_count -= 2
+rows_count -= 2		# MOVE BACK TO ALIGN BUTTONS TO BE ADDED
 
-reset_button = Button(canvas, text = "Reset values") 	# RESET BUTTON 
+reset_button = Button(canvas, text = "Reset values", command = reset) 	# RESET BUTTON 
 reset_button.grid(row = rows_count, column = 4, pady = 5, sticky = N+W+S+E)
 
-check_button = Button(canvas, text = "Check inventory")	# CHECK BUTTON
+check_button = Button(canvas, text = "Check inventory", command = check)	# CHECK BUTTON
 check_button.grid(row = rows_count + 1, column = 4, pady = 5, sticky = N+W+S+E)
 
 # quit_button = Button(canvas, text = "Quit program") 	# QUIT BUTTON
