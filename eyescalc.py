037763acd69e10b8ce62248730726675e3f00293
aalin <aaron.lin@stonybrook.edu>
2016-12-26 17:12:05 -0500

    Working on checking lock solutions

--- eyescalc.py
+++ eyescalc.py
@@ -7,47 +7,80 @@
 # HOW TO DISPLAY SOLUTION
 # HOW TO GRAB ALL VALUES AND ASSIGN THEM THE CORRECT SHAPE+COLOR VALUE?
 # validatecommand for SpinBox
-# command for button
-# selection_clear
 
 # THIS IS FOR CHECKING IF CURRENT INVENTORY HAS SOLUTION
 # WHAT IF PERSON HAS ITEMS THAT CAN BE CHANGED TO A SOLUTION?
 # USE OF TUPLES, LISTS, DICTIONARY
 
+# THIS CAN BE DONE IN A MORE EFFICIENT ALGORITHM BUT I CANT THINK OF ONE
+
 try:
-    # Python 3.x
-    from tkinter import * 
-except ImportError:
     # Python 2.x
-    from Tkinter import *
+    from Tkinter import * 
+except ImportError:
+	# Python 3.x
+    from tkinter import *
 from PIL import Image, ImageTk
 
 colors = ['Red','Orange','Yellow','Green','Blue', 'Indigo', 'Violet']
 shapes = ['circle', 'triangle', 'square', 'pentagon']
-values = {'Red_circle':1, 'Orange_circle':2, 'Yellow_circle':3, 'Green_circle':4, \
-		'Blue_circle':5, 'Indigo_circle':6, 'Violet_circle':7, \
-		'Red_triangle':3, 'Orange_triangle':6, 'Yellow_triangle':9, 'Green_triangle':12, \
-		'Blue_triangle':15, 'Indigo_triangle':18, 'Violet_triangle':21, \
-		'Red_square':4, 'Orange_square':8, 'Yellow_square':12, 'Green_square':16, \
-		'Blue_square':20, 'Indigo_square':24, 'Violet_square':28, \
-		'Red_pentagon':5, 'Orange_pentagon':10, 'Yellow_pentagon':15, \
-		'Green_pentagon':20, 'Blue_pentagon':25, 'Indigo_pentagon':30, 'Violet_pentagon':35}
+values = [('Red_circle',1), 	('Orange_circle',2),	('Yellow_circle',3), 	('Green_circle',4),
+		('Blue_circle',5), 		('Indigo_circle',6), 	('Violet_circle',7),
+		('Red_triangle',3), 	('Orange_triangle',6), 	('Yellow_triangle',9), 	('Green_triangle',12), 
+		('Blue_triangle',15), 	('Indigo_triangle',18), ('Violet_triangle',21),
+		('Red_square',4), 		('Orange_square',8), 	('Yellow_square',12), 	('Green_square',16), 
+		('Blue_square',20), 	('Indigo_square',24), 	('Violet_square',28), 
+		('Red_pentagon',5), 	('Orange_pentagon',10), ('Yellow_pentagon',15), ('Green_pentagon',20), 
+		('Blue_pentagon',25), 	('Indigo_pentagon',30), ('Violet_pentagon',35)]
 
 def reset():
 	# ITERATE THROUGH THE DATA STUCTURE HOLDING ALL SPINBOXES AND ENTRY
 	# RESET ALL VALUES THEY HOLD
 	for widgets in spin_widgets:
-		widgets.delete(0, END)		# CLEARS WIDGET VALUEa
+		widgets.delete(0, END)		# CLEARS WIDGET VALUE
 		widgets.insert(0, 0)		# RESET TO 0
 	for widgets in entry_widgets:
 		widgets.delete(0, END)
 
 def check():
-	for widgets in spin_widgets:
-		spin_countwidget.gets()
+	for widgets, index in enumerate(spin_widgets):
+		spin_counts[index] = widget.get()			# GET SPIN BOX VALUES AND STORE INTO INDEX OF LIST OF COUNTERS
+	for widgets, index in enumerate(entry_widgets):
+		entry_counts[index] = widget.get()
+
+	i = 0		# ITERATE THROUGH SPIN COUNTS
+	j = 0		# ITERATE THROUGH ENTRY COUNTS
+	for s in shapes:
+		for c in colors:
+			key = c + '_' + s
+			shape_counts[key] = spin_counts[i]	# STORE SHAPE COUNT INTO DICTIONARY
+			i += 1
+
+	for entry, index in enumerate(entry_counts):	# GO THROUGH THE LOCKS AND TRY TO FIND SOLUTION FROM SHAPE COUNTS
+		for s in shapes:
+			for c in colors:
+				key = c + '_' + s
+				print key
+
+
+
+	# SO I HAVE THE COUNT OF EVERY SPIN BOX STORED INTO COUNT LISTS
+	# NOW I NEED TO CHECK IF THEY HAVE SOLUTION...
+	# I NEED TO MULTIPLY 
+	# WHAT IF I HAVE TWO LISTS OF TUPLES...
+	# ITERATE THROUGH SHAPES AND COLORS NESTED FOR LOOP
+	# CALL LIST OF TUPLES THROUGH THAT FOR LOOP...
+
 
 	# CHECK ALL SPINBOXES AND SEES IF WE HAVE SHAPES THAT SOLVE EACH LOCK
-	print "check"
+
+	# OKAY, RATHER THAN THAT. WE USE A LIST OF TUPLES FOR SHAPE VALUES
+	# AND A DICTIONARY FOR THE SHAPE COUNTS
+	# NOW WE NEED TO GO THROUGH THE LOCKS 
+	# FIRST LOCK = 1 SHAPE NEEDED
+	# SECOND LOCK = 1 SHAPE NEEDED
+	# THIRD LOCK = 2 SHAPES NEEDED
+	# FOURTH LOCK = 3 SHAPES NEEDED
 
 
 root = Tk()
@@ -56,15 +89,23 @@ root.title("Eyes of Glouphrie Calculator")
 frame = Frame(root)
 frame.pack()
 
-canvas = Canvas(frame, bg="grey23", width=1500, height=2000)
+canvas = Canvas(frame, bg = "grey23", width = 1500, height = 2000)
 canvas.pack()
 
 rows_count = 0
 columns_count = 0
 spin_widgets = []		# STRUCTURE TO HOLD ALL SPIN BOX WIDGETS
 entry_widgets = []		# STRUCTURE TO HOLD ALL ENTRY WIDGETS
-spin_counts = [0] * 28	# INITIALIZE THE A
-entry_counts = [0] * 4
+spin_counts = (0) * 28	# LIST OF COUNTS BASED ON SPIN BOXES
+entry_counts = (0) * 4	# VALUES OF THE LOCKS (FIRST TO FOURTH)
+shape_counts = {'Red_circle':0, 'Orange_circle':0,		'Yellow_circle':0,		'Green_circle':0,
+			'Blue_circle':0, 	'Indigo_circle':0, 		'Violet_circle':0,
+			'Red_triangle':0, 	'Orange_triangle':0, 	'Yellow_triangle':0,	'Green_triangle':0, 
+			'Blue_triangle':0, 	'Indigo_triangle':0, 	'Violet_triangle':0,
+			'Red_square',0, 	'Orange_square':0, 		'Yellow_square':0,		'Green_square':0, 
+			'Blue_square':0, 	'Indigo_square':0, 		'Violet_square':0, 
+			'Red_pentagon':0, 	'Orange_pentagon':0,	'Yellow_pentagon':0,	'Green_pentagon':0, 
+			'Blue_pentagon':0, 	'Indigo_pentagon':0, 	'Violet_pentagon':0}
 
 # IMAGES AND SPIN BOXES
 for s in shapes:
@@ -97,7 +138,6 @@ for s in shapes:
 
 	rows_count += 1
 	columns_count = 0
-
 
 # LABEL FOR LOCKS
 first_lock = Label(canvas, text = "First Lock:		")
